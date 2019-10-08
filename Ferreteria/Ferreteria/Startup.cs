42bac191d807300a5ed5c5a8671c8ea41c5eb1f6
miguelRGz <56096424+miguelRGz@users.noreply.github.com>
2019-10-08 12:18:50 -0500

    configuracion de diseño

--- Ferreteria/Ferreteria/Startup.cs
+++ Ferreteria/Ferreteria/Startup.cs
@@ -25,7 +25,7 @@ namespace Ferreteria
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "Ferreteria/dist";
             });
         }
 
@@ -59,7 +59,7 @@ namespace Ferreteria
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "Ferreteria";
 
                 if (env.IsDevelopment())
                 {
